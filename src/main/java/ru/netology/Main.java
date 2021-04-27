edde37cef2fb336b86c15ece609b3353223ec13f
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-04-27 16:31:22 +0300

    commit 27.04.21

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,24 +1,103 @@
 package ru.netology;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Product product = new Product();
+        Scanner scanner = new Scanner(System.in);
+
 
         /**
          * Наполняем магазин продуктами
          */
         product.addBaseProduct();
-
-        /**
-         * Выводим список продуктов на экран
-         */
-        System.out.println("Список возможных товаров для покупки");
         product.getProductList();
-        System.out.println("\n-------------------------------\n");
 
-        product.filterProductName("Молоко");
-        System.out.println("\n-------------------------------\n");
+        product.addProductBasket( "Соль", 4);
+        System.out.println("----------------------------------------");
+        product.getProductBasket();
+        product.addProductBasket( "Морковь", 5);
+        System.out.println("----------------------------------------");
+        product.getProductBasket();
+        System.out.println("----------------------------------------");
+        product.clearBasket();
+        product.getProductBasket();
 
-        product.filterProductPrice(10, 40);
+
+//        System.out.println("Добро пожаловать в магазин!");
+//
+//        System.out.println("Выберите необходимый пункт в меню:\n" +
+//                "1. Вывести список продуктов\n" +
+//                "2. Добавить в корзину\n" +
+//                "3. Оплатить продукты в корзине\n" +
+//                "4. Показать продукты в корзине\n" +
+//                "5. Удалить из корзины\n" +
+//                "6. Фильтровать услуги по цене\n" +
+//                "7. Найти товар по наименованию\n" +
+//                "8. Проверить заказ по трекинг номеру\n" +
+//                "9. Повторить заказ по трекинг номеру\n" +
+//                "10. Завершить работу программы");
+//
+//        innerloop:
+//        while (true) {
+//            System.out.print("\nПоле ввода: ");
+//            int inputUser = scanner.nextInt();
+//
+//            switch (inputUser) {
+//                case 1 -> {
+//                    System.out.println("Список доступных товаров:\n");
+//                    product.getProductList();
+//                }
+//                case 2 -> {
+//                    Scanner scannerBasket = new Scanner(System.in);
+//                    product.getProductList();
+//                    System.out.println("\nВведите имя продукта из списка и его количество, " +
+//                            "который хотите добавить в корзину");
+//                    String products = scannerBasket.nextLine();
+//                    String[] parts = products.split(" ");
+//                    String productName = parts[0];
+//                    int productQuantity = Integer.parseInt(parts[1]);
+//                    product.addProductBasket(product.getProduct(), productName, productQuantity);
+//                }
+//                case 3 -> {
+//                    // Оплатить продукты в корзине
+//                }
+//                case 4 -> {
+//                    System.out.println("Список продуктов в корзине:");
+//                    product.getProductBasket();
+//                }
+//                case 5 -> {
+//                    product.clearBasket();
+//                    System.out.println("Корзина успешно очищена");
+//                }
+//                case 6 -> {
+//                    System.out.print("Введите мин. цену продукта: ");
+//                    int minValue = scanner.nextInt();
+//                    System.out.print("Введите макс. цену продукта: ");
+//                    int maxValue = scanner.nextInt();
+//                    product.filterProductPrice(minValue, maxValue);
+//                }
+//                case 7 -> {
+//                    String productName = scanner.nextLine();
+//                    product.filterProductName(productName);
+//                }
+//                case 8 -> {
+//                    // Проверить заказ по трекинг номеру
+//                }
+//                case 9 -> {
+//                    // Повторить заказ по трекинг номеру
+//                }
+//                case 10 -> {
+//                    System.out.println("Вы вышли из программы.");
+//                    break innerloop;
+//                }
+//
+//                default -> System.out.println("Выберите корректный пункт из меню");
+//
+//            }
+//
+//        }
+//        scanner.close();
     }
 }
--- src/main/java/ru/netology/Product.java
+++ src/main/java/ru/netology/Product.java
@@ -2,13 +2,18 @@ package ru.netology;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Product {
-
     private Map<String, Integer> products;
+    private Map<String, Integer> productsBasket;
+    //private Map<Product, Integer> productsBasket;
+    private Map<String, Integer> createdOrder;
 
     public Product() {
         products = new HashMap<>();
+        productsBasket = new HashMap<>();
+        createdOrder = new HashMap<>();
     }
 
 
@@ -26,33 +31,65 @@ public class Product {
 
 
     /**
-     * Метод выводит весь список товаров
+     * Возвращает мапу продуктов
+     */
+    public Map getProduct() {
+        Set<String> setKeys = products.keySet();
+        for (String k : setKeys) {
+            products.get(k);
+        }
+        return products;
+    }
+
+    /**
+     * Метод выводит весь список товаров клиенту
      */
     public void getProductList() {
         int i = 1;
         for (Map.Entry<String, Integer> product : products.entrySet()) {
-            System.out.println(i++ + ". " + "Название: " + product.getKey() +
-                    ", Цена: " + product.getValue() + "р за ед.");
+            System.out.println(i++ + ". " + product.getKey() + ", " + product.getValue() + " руб. за ед.");
         }
     }
 
-    public void filterProductName(String name) {
-        int i = 1;
+    public void clearBasket() {
+        productsBasket.clear();
+    }
 
+    public void addProductBasket(String productName, int quantity) {
         for (Map.Entry<String, Integer> product : products.entrySet()) {
-            if (name.equals(product.getKey())) {
-                System.out.println(i++ + ". " + "Название: " + product.getKey() +
-                        ", Цена: " + product.getValue() + "р за ед.");
+            if (product.getKey().equals(productName)) {
+                productsBasket.put(productName, product.setValue(quantity));
+                break;
             }
         }
     }
 
-    public void filterProductPrice(Integer minValue, Integer maxValue) {
+
+    public void getProductBasket() {
+        int i = 1;
+        if (productsBasket.isEmpty()) {
+            System.out.println("Корзина пуста");
+        } else {
+            for (Map.Entry<String, Integer> product : productsBasket.entrySet()) {
+                System.out.println(i++ + ". " + product.getKey() + ", " + product.getValue() + " шт.");
+            }
+        }
+    }
+
+
+    public void filterProductName(String name) {
+        for (Map.Entry<String, Integer> product : products.entrySet()) {
+            if (name.equals(product.getKey())) {
+                System.out.println(product.getKey() + ", " + product.getValue() + " руб. за ед.");
+            }
+        }
+    }
+
+    public void filterProductPrice(int minValue, int maxValue) {
         int i = 1;
         for (Map.Entry<String, Integer> product : products.entrySet()) {
             if (product.getValue() >= minValue && product.getValue() <= maxValue) {
-                System.out.println(i++ + ". " + "Название: " + product.getKey() +
-                        ", Цена: " + product.getValue() + "р за ед.");
+                System.out.println(i++ + ". " + product.getKey() + ", " + product.getValue() + " руб. за ед.");
             }
         }
     }
@@ -61,6 +98,8 @@ public class Product {
      * Метод добавляет основной список продуктов в магазин. Реализовано тут, чтобы не захламлять Main.
      */
     public void addBaseProduct() {
+        products.put("Лук", 18);
+        products.put("Перец", 6);
         products.put("Свекла", 57);
         products.put("Яйцо", 25);
         products.put("Картофель", 13);
